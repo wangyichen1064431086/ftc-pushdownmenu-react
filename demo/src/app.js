3ec3a7de4912634b09592b433d4c3ff8b1623061
wangyichen1064431086 <1064431086@qq.com>
2018-07-24 16:38:05 +0800

    Some modify

--- demo/src/app.js
+++ demo/src/app.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <PushdownMenu>
-    <PushdownItem name={"简体中文"} url={"http://www.ftchinese.com"} />
+    <PushdownItem name={"简体中文"}/>
     <PushdownItem name={"繁体中文"} url={"http://big5.ftchinese.com/"} />
     <PushdownItem name={"英文"} url={"https://www.ft.com/"} />
   </PushdownMenu>,
